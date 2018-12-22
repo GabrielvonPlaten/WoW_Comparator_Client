4f224294e984721757cc7d4a9749cd67fb57a683
Gabriel von Platen <gabrielvonplaten@gmail.com>
2018-12-22 02:04:04 +0100

    Add Sass globaly to the project

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,10 @@ const path = require('path');
 
 module.exports = {
   outputDir: path.resolve(__dirname, '../server/public'),
+  pluginOptions: {
+    "style-resources-loader": {
+      preProcessor: "sass",
+      patterns: [path.resolve(__dirname, "./src/sass/global.sass")]
+    }
+  }
 }
