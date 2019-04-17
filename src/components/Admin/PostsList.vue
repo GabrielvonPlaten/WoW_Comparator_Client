f1599ee084928ddc9927c0970b9b3805c0f96f84
Gabriel von Platen <gabrielvonplaten@gmail.com>
2019-04-17 05:09:03 +0200

    Small css improvements for the post form & add view all posts view in the dashboard

---
+++ src/components/Admin/PostsList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="postlists-container">
+      <div class="postslist-title">
+        <h2>All Posts</h2>
+      </div>
+      <div>
+        <ul></ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      posts: [],
+    }
+  },
+
+  created() {
+    let url = '/api/posts'
+    axios.get(url)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err))
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
+
+
--- src/views/Admin/Dashboard.vue
+++ src/views/Admin/Dashboard.vue
@@ -1,18 +1,13 @@
 <template>
   <div class="admin-dashboard-bg">
-    <form @submit.prevent="submitPost()">
-      <div class="form-division">
-        <label for="title">Title</label>
-        <input v-model="postContent.title" name="title">
-      </div>
-      <div class="form-division">
-        <label for="subtitle">Subtitle</label>
-        <input v-model="postContent.subtitle" name="subtitle">
-      </div>
-      <div id="editorjs"></div>
-      <button class="btn btn--green btn-form-submit">Submit</button>
-    </form>
-
+    <div>
+      <ul>
+        <li><button @click="switchView(1)">View All Posts</button></li>
+        <li><button @click="switchView(0)" >Add Post</button></li>
+      </ul>
+    </div>
+    <PostsList v-if="views[0].active"/>
+    <PostForm v-if="views[1].active" />
     <button @click.prevent="logoutAdmin" class="btn btn--red logout-btn">Logout</button>
   </div>
 </template>
@@ -22,68 +17,23 @@ import axios from 'axios';
 import adminService from '@/apis/adminService';
 import store from '@/Vuex/store'
 
-import EditorJS from '@editorjs/editorjs';
-import Header from '@editorjs/header';
-import List from '@editorjs/list';
-import Embed from '@editorjs/embed';
-import SimpleImage from '@editorjs/simple-image';
-
-
+// Components
+import PostForm from '@/components/Admin/PostForm.vue'
+import PostsList from '@/components/Admin/PostsList.vue'
 
 export default {
   data() {
     return {
-      postContent: {
-        title: "",
-        subtitle: "",
-      },
-      editor: new EditorJS({
-        holderId: 'editorjs',
-        tools: {
-          header: {
-            class: Header,
-            inlineToolBar: ['link']
-          },
-
-          list: {
-            class: List,
-            inlineToolbar: ['link', 'bold']
-          },
-
-          embed: {
-            class: Embed,
-            inlineToolbar: false,
-            config: {
-              services: {
-                youtube: true,
-                coub: true,
-              }
-            }
-          },
-
-          image: {
-            class: SimpleImage,
-          }
-        },
-      })
+      createPostView: true,
+      showPostView: false,
+      views: [
+        { id: 0, name: "Posts", active: false },
+        { id: 1, name: "Create Post", active: true }
+      ]
     }
   },
 
   methods: {
-    submitPost() {
-      console.log(this.editor);
-      let title = this.postContent.title;
-      let subtitle = this.postContent.subtitle
-
-      this.editor.save()
-        .then(outputData => {
-          axios.post('/api/create-post', {title, subtitle, outputData})
-            .then(res => console.log(res.data))
-            .catch(err => console.log(err));
-      })
-      .catch(err => console.log(err));
-    },
-
     logoutAdmin() {
       adminService.logout(localStorage.getItem('token'))
         .then(async res => {
@@ -91,8 +41,23 @@ export default {
           store.dispatch('adminLogout');
           this.$router.go({ path: this.$router.path })
         })
+    },
+
+    switchView(x) {
+      this.views.forEach(view => {
+        if (view.id === x) {
+          view.active = true
+        } else {
+          view.active = false
+        }
+      })
     }
   },
+
+  components: {
+    PostForm,
+    PostsList,
+  }
 }
 </script>
 
@@ -101,31 +66,6 @@ export default {
 .dashboard-greet-message
   color: $white-2
 
-form
-  width: 70%
-  padding: 1rem 3rem
-  margin: 1.5rem auto
-  background: $blue-6
-  border-radius: 0.4rem
-
-  .form-division
-    margin: 1.2rem 0
-
-    label
-      display: block
-      color: $white-4
-      font-size: 1.2rem
-      font-weight: 300
-
-  .btn-form-submit
-    margin: 1rem 0
-
-#editorjs
-  color: $white-2
-  border: 1px solid $blue-4
-  background: $blue-7
-  margin: 0 auto
-  min-height: 70vh
 </style>
 
 
